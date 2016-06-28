feca346beed3a7067b1a332522304161afa016f8
Rafastaria <guitronleafar@gmail.com>
2016-06-28 20:13:07 +0000

    second commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cubosApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cubosApp
+ */
+angular.module('cubosApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cubosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cubosApp
+ */
+angular.module('cubosApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
